92f37a2d8f1097d8b09b99e0520986d1f720d7a6
Victor Guennou <victor.guennou@uavia.eu>
2021-03-16 14:59:59 +0100

    Remove comments' effect since they are unused for now...

--- app/components/Statements/StatementsGrid.jsx
+++ app/components/Statements/StatementsGrid.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { classifyComments } from '../../state/video_debate/comments/selectors'
 import StatementCard from './StatementCard'
 import styled from 'styled-components'
-
-import {
-  joinCommentsChannel,
-  joinCommentsTestChannel,
-  leaveCommentsChannel,
-} from '../../state/video_debate/comments/effects'
-import {
-  joinStatementsChannel,
-  leaveStatementsChannel,
-} from '../../state/video_debate/statements/effects'
 
 const StatementsList = styled.div`
   border-top: 1px solid #e8e8e8;
@@ -29,24 +18,7 @@ const StatementsList = styled.div`
   }
 `
 
-@connect((state) => ({}),
-  {
-    joinCommentsTestChannel,
-    leaveCommentsChannel,
-  }
-)
 export class StatementsGrid extends React.PureComponent {
-  componentDidMount() {
-    // Join channels
-    this.props.joinCommentsTestChannel()
-    //this.props.joinCommentsChannel(videoId)
-  }
-
-  componentWillUnmount() {
-    this.props.leaveCommentsChannel()
-    //this.props.leaveStatementsChannel()
-  }
-
   render() {
     return (
       <StatementsList className="columns is-multiline">
